6992899016b1a890da267bfe8c4c9101ecfe97de
karolmalyszko <karol.malyszko@torneo.eu>
2016-08-08 22:51:58 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,2 @@
+sudo apt-get install libglib2.0-dev --fix-missing -y
+sudo apt-get install libusb-dev libudev-dev libical-dev libreadline-dev libdbus-glib-1-dev bluetooth bluez blueman python-bluez git -y
