9938b85423444c2a43b62f208f6b73ce52888ea1
karol <karol@DESKTOP-G8MQPBF>
2016-08-11 09:09:59 +0200

    set real-time value for transmitter power in FSPL algorithm

--- iBeaconReport.py
+++ iBeaconReport.py
@@ -54,14 +54,14 @@ class iBeaconReport:
     def setDistanceFSPL(self):
         # Free-space path loss method
         K = -27.55 # constant dependant of distance unit -> 32.44 if distance in [km], -27.55 if distance in [m]
-        Ptx = -15 # transmitter power; between -30dBm and +4dBm; unknown at the moment
+        Ptx = -4 # transmitter power; between -30dBm and +4dBm; configurable via cloud.estimote.com
         Prx = self.measuredPower # receiver sensitivity
         CLtx = 0 # transmitter cable loss; 0 if no cables
         CLrx = 0 # receiver cable loss; 0 if no cables
         AGtx = 0 # transmitter antenna gain; assume 0
         AGrx = 0 # receiver antenna gain; assume 0
         FM = 22 # fade margin; assume 22 for no obstacles between transmitter and receiver
-        f = 2400 # signal frequency -> 2.4 GHz for bluetooth; has to be in MHz, so assume 2400
+        f = 2400 # signal frequency -> 2.4 GHz for bluetooth; has to be in MHz
 
         FSPL = Ptx - CLtx + AGrx + AGtx - CLrx - Prx - FM # free-space path loss value
         self.accuracy = math.pow( 10, (FSPL - K - (20*math.log10(f)) ) / 20 )
